df177030bdbc1539d26a870e6474c2cf8e696643
Soufiane AIT ELGHAZI <soufiane.aitelghazi@hps-worldwide.com>
2025-01-25 15:54:19 +0100

    first commit

---
+++ src/test/java/org/skypay/bankkata/BankKataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypay.bankkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
