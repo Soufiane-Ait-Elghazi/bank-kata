a71f8144ee502f514c65b4abfb76940db6b10ec3
Soufiane AIT ELGHAZI <soufiane.aitelghazi@hps-worldwide.com>
2025-01-25 16:44:42 +0100

    AccountService Methods implimentation

--- src/main/java/org/skypay/bankkata/services/Account.java
+++ src/main/java/org/skypay/bankkata/services/Account.java
@@ -1,11 +1,15 @@
 package org.skypay.bankkata.services;
 
 import org.skypay.bankkata.entities.Transaction;
+import org.skypay.bankkata.enums.TransactionType;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Account implements AccountService{
+import static org.skypay.bankkata.enums.ErrorCode.*;
+import static org.skypay.bankkata.enums.ErrorCode.STATEMENT_FORMAT;
+
+public class Account implements AccountService {
     private int balance = 0;
     StringBuffer statementBuffer ;
     private final List<Transaction> transactions = new ArrayList<>();
@@ -21,18 +25,48 @@ public class Account implements AccountService{
     public List<Transaction> getTransactions() {
         return transactions;
     }
+
     @Override
     public void deposit(int amount) {
-        throw new UnsupportedOperationException("deposit method not implemented yet.");
+        if (amount <= 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT.getText());
+        }
+        balance += amount;
+        transactions.add(new Transaction(TransactionType.DEPOSIT, amount, balance));
     }
 
     @Override
     public void withdraw(int amount) {
-        throw new UnsupportedOperationException("withdraw method not implemented yet.");
+        if (amount <= 0) {
+            throw new IllegalArgumentException(INVALID_AMOUNT.getText());
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException(INSUFFICIENT_FUNDS.getText());
+        }
+        balance -= amount;
+        transactions.add(new Transaction(TransactionType.WITHDRAW, amount, balance));
     }
 
     @Override
     public void printStatement() {
-        throw new UnsupportedOperationException("printStatement method not implemented yet.");
+        StringBuffer statementBuffer = new StringBuffer();
+        statementBuffer.append(PRINT_STATEMENT_HEADER.getText()).append("\n");
+        for (Transaction transaction : transactions) {
+            String formattedAmount = transaction.getType().equals(TransactionType.WITHDRAW)
+                    ? "-" + transaction.getAmount()
+                    : String.valueOf(transaction.getAmount());
+
+            statementBuffer.append(String.format(STATEMENT_FORMAT.getText(),
+                            transaction.getDate(),
+                            formattedAmount,
+                            transaction.getBalanceAfter()))
+                    .append("\n");
+        }
+        this.statementBuffer = statementBuffer;
+        System.out.println(statementBuffer.toString());
     }
+
+
+
+
 }
